384d4a025420115d22d91e4f2e46695cc82ca7e5
Your Name <you@example.com>
2017-04-20 17:25:34 +0200

    ya les deux prog de cette semaine et de la seance avt les vac j'ai oublier dsl

--- TP1.2/src/main.cpp
+++ TP1.2/src/main.cpp
@@ -1,39 +1,64 @@
 
 #include "jpg.h"
 #include "mnist.h"
-
-float dist(float* v1 , float* v2)
-{
-  	float distance =0;
-	for(int i=0; i<784 ; i++){
-		
-		distance = (v1[i]-v2[i]*v1[i]-v2[i]);
+#include <limits.h>
+#include <time.h>
+float dist(float* v1, float* v2){
+	int i;
+	float d=0;
+	for (int i=0; i<784; i++){
+		d+= (v1[i]-v2[i])*(v1[i]-v2[i]);		
 	}
-	return distance ;
-	//return 
+	return d;
+}
+
+float linear_classifier(float* w, float* x){
+	float d=0;
+	for (int i=0; i<784; i++){
+		d+= w[i]*x[i];
+	}if (d>=0) {return 1;
+	}return -1;
 }
 
 
-
-int main()
-{
-  
-	
+int main(){
+	srand(time(NULL));
+ 	float gamma =0.01;
+	float** images = read_mnist("train-images.idx3-ubyte");
 	float* labels = read_labels("train-labels.idx1-ubyte");
-    float** images = read_mnist("train-images.idx3-ubyte");
-	float** teste_images = read_mnist("t10k-images.idx3-ubyte");
-    float mind =-1; 
-	int inference,NN;
-	int j , i;
-	for(i=0; i<10000; i++) {
-       	for(j=0; j<60000; j++) {
-		 float d= dist(teste_images[i],images[j]);
-		 if(d<mind || mind == -1){mind=d; NN=j;}
-		}
-}	 
-inference = labels[NN];
+	float** test_images = read_mnist("t10k-images.idx3-ubyte");
+	float* test_labels = read_labels("t10k-labels.idx1-ubyte");
+	float* w = new float[784];
+	for (int i=0; i<784; i++) {
+		w[i]=(float) rand()*2/INT_MAX-1; 	
+	}
 
-        save_jpg(teste_images[i], 28, 28, "%u/%04u.jpg", inference, i);
-    
+for(int i=0;i<60000;i++){
+	printf("%u\n",i);	
+	int prediction = linear_classifier (w, images[i] 	);
+	
+	int verite= (labels[i]==1) ? 1 :-1;
+if (verite!= prediction){
+printf("erreur \n");
+for(int j=0;j<784;j++) w[j]=w[j] + gamma*verite*images [i] [j] ;
+}
+	}
+float E=0;
+
+
+    for(int i=0; i<10000; i++) {
+		printf("%u\n", i);
+		int NN;
+		int inference	= linear_classifier (w, test_images[i] 	);
+		save_jpg(test_images[i], 28, 28, "%u/%u.jpg", inference, i);
+		
+		//if (inference != test_labels[i]){ 
+			if ( (inference==1 && test_labels[i]!=1 ) || (inference == -1 && test_labels[i]==1)) E++;
+		
+			
+		
+	printf ("erreur= %0.2f%%\n", (E*100)/i);
+	}
+
     return 0;
 }

17f26a40c25511f9897c18e128ce2e61c58c089f
merabetam <mamar78.95@gmail.com>
2017-04-20 14:14:54 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,48 @@
+
+#include "jpg.h"
+#include "mnist.h"
+#include <limits.h>
+
+float dist(float* v1, float* v2){
+	int i;
+	float d=0;
+	for (int i=0; i<784; i++){
+		d+= (v1[i]-v2[i])*(v1[i]-v2[i]);		
+	}
+	return d;
+}
+
+float linear_classifier(float* w, float* x){
+	float d=0;
+	for (int i=0; i<784; i++){
+		d+= w[i]*x[i];
+	}if (d>=0) {return 1;
+	}return 0;
+}
+
+
+int main(){
+	float** images = read_mnist("train-images.idx3-ubyte");
+	float* labels = read_labels("train-labels.idx1-ubyte");
+	float** test_images = read_mnist("t10k-images.idx3-ubyte");
+	float* test_labels = read_labels("t10k-labels.idx1-ubyte");
+	float* w = new float[784];
+	for (int i=0; i<784; i++) {
+		w[i]=(float) rand()*2/INT_MAX-1; 	
+	}float E=0;
+
+    for(int i=0; i<10000; i++) {
+		printf("%u\n", i);
+		int NN;
+		}int inference = linear_classifier (w, test_images[i]);
+		//save_jpg(test_images[i], 28, 28, "%u/%u.jpg", inference, i);
+		
+		if (inference != test_labels[i]){ 
+			E++;
+			
+		}
+	printf ("erreur= %0.2f%%\n", (E*100)/i);
+	}
+
+    return 0;
+}
